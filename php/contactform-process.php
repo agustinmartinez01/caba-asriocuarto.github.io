73c529ebf5351b6797a2cdf137354a276da663b2
agustinmartinez <agustinmartinez01@gmail.com>
2021-02-20 01:12:31 -0300

    cambios y agregado de envio de consultas

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -1,4 +1,8 @@
 <?php
+
+require("class.phpmailer.php");
+require("class.smtp.php");
+
 $errorMSG = "";
 
 if (empty($_POST["name"])) {
@@ -19,33 +23,61 @@ if (empty($_POST["message"])) {
     $message = $_POST["message"];
 }
 
+// Valores enviados desde el formulario
+if ( !isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]) ) {
+    $errorMSG = "Es necesario completar todos los datos del formulario";
+}
+$nombre = $_POST["name"];
+$mensaje = $_POST["message"];
+$email = $_POST["email"];
+$mensajeFinal ="Correo PaginaWeb de ".$nombre." Email: ".$email."\n"." Mando el mensaje ". $mensaje;
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = " c1980739.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "no-reply@c1980739.ferozo.com";  // Mi cuenta de correo
+$smtpClave = "a4WQ3c74ic";  // Mi contraseña
 
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "agustinmartinez01@gmail.com";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 587; 
+$mail->SMTPDebug = 3;
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+$mail->AddReplyTo($email); // Esto es para que al recibir el correo y poner Responder, lo haga a la cuenta del visitante. 
+$mail->Subject = "formulario de contacto"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensajeFinal);
+$mail->Body = "{$mensajeHtml} <br /><br />Formulario de contacto<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Formulario de contacto"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
 
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
-    }
+if ($mail->send()) {
+    echo "success";
+} else {
+    echo "Fallo";
+    
 }
+
+
 ?>
